288d968084ed644779581f343f9b8a93a17d18cd
yungity <yunhasry@gmail.com>
2019-09-11 08:05:47 +0900

    bmp open and save

---
+++ bmp_kyh_2Doc.cpp
@@ -0,0 +1,244 @@
+﻿
+// bmp_kyh_2Doc.cpp: Cbmpkyh2Doc 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "bmp_kyh_2.h"
+#endif
+
+#include "bmp_kyh_2Doc.h"
+# include <memory.h>
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// Cbmpkyh2Doc
+
+IMPLEMENT_DYNCREATE(Cbmpkyh2Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(Cbmpkyh2Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// Cbmpkyh2Doc 생성/소멸
+
+Cbmpkyh2Doc::Cbmpkyh2Doc() noexcept
+{
+	// TODO: 여기에 일회성 생성 코드를 추가합니다.
+
+}
+
+Cbmpkyh2Doc::~Cbmpkyh2Doc()
+{
+}
+
+BOOL Cbmpkyh2Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 여기에 재초기화 코드를 추가합니다.
+	// SDI 문서는 이 문서를 다시 사용합니다.
+
+	return TRUE;
+}
+
+
+
+
+// Cbmpkyh2Doc serialization
+
+void Cbmpkyh2Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 여기에 저장 코드를 추가합니다.
+	}
+	else
+	{
+		// TODO: 여기에 로딩 코드를 추가합니다.
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// 축소판 그림을 지원합니다.
+void Cbmpkyh2Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// 문서의 데이터를 그리려면 이 코드를 수정하십시오.
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// 검색 처리기를 지원합니다.
+void Cbmpkyh2Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// 문서의 데이터에서 검색 콘텐츠를 설정합니다.
+	// 콘텐츠 부분은 ";"로 구분되어야 합니다.
+
+	// 예: strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void Cbmpkyh2Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// Cbmpkyh2Doc 진단
+
+#ifdef _DEBUG
+void Cbmpkyh2Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void Cbmpkyh2Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// Cbmpkyh2Doc 명령
+
+
+BOOL Cbmpkyh2Doc::OnOpenDocument(LPCTSTR lpszPathName)
+{
+	if (!CDocument::OnOpenDocument(lpszPathName))
+		return FALSE;
+
+	// TODO:  여기에 특수화된 작성 코드를 추가합니다.
+	//CFile에서 처리
+	CFile hFile;        //파일을 읽어들이면 이 클래스 형식으로 저장.
+
+	int height;        //이미지의 높이를 저장할 변수
+	int width;        //이미지의 가로길이를 저장할 변수
+
+	hFile.Open(lpszPathName, CFile::modeRead | CFile::typeBinary);    //파일 열기
+	hFile.Read(&dibHf, sizeof(BITMAPFILEHEADER));                          //dibHf에 파일헤더를 읽어들인다.
+
+	//이 파일이 BMP파일인지 검사 
+	if (dibHf.bfType != 0x4D42)
+	{
+		AfxMessageBox(L"Not BMP file!!");                                        //프로젝트 생성시 유니코드를 사용하게 할 경우
+		return FALSE;                                                                      //L을 붙여준다
+	}
+
+	hFile.Read(&dibHi, sizeof(BITMAPINFOHEADER));                             //영상정보의 header를 읽기
+	if (dibHi.biBitCount != 8 && dibHi.biBitCount != 24)                                //8,24비트가 아닐경우
+	{
+		AfxMessageBox(L"Gray/True Color Possible!!");
+		return FALSE;
+	}
+
+	if (dibHi.biBitCount == 8)
+		//8비트의 경우 팔레트를 생성해 주어야 한다. 총 256가지 색이므로 그 길이만큼 읽어들인다
+		hFile.Read(palRGB, sizeof(RGBQUAD) * 256);
+
+	//메모리 할당
+
+	if (dibHi.biBitCount == 8)
+	{
+		ImgSize = hFile.GetLength() - sizeof(BITMAPFILEHEADER) - sizeof(BITMAPINFOHEADER) - 256 * sizeof(RGBQUAD);
+		//이미지의 크기는 파일 총 길이에서, 두 헤드와 팔레트의 사이즈를 제외한다.
+	}
+	else if (dibHi.biBitCount == 24) //컬러영상
+	{
+		ImgSize = hFile.GetLength() - sizeof(BITMAPFILEHEADER) - sizeof(BITMAPINFOHEADER);
+	}
+	
+	//이미지의 길이정보
+	height = dibHi.biHeight;
+	width = dibHi.biWidth;
+	m_InImg = new unsigned char[ImgSize];
+	m_OutImg = new unsigned char[ImgSize];
+
+	//이미지를 저장, 출력할 배열생성.
+	/*m_InImg = (unsigned char**) malloc(ImgSize);
+	for (int i = 1; i < height; i++) {
+		m_InImg[i] = m_InImg[i-1] + width;
+	}
+	memset(m_InImg[0], 0, ImgSize);
+	
+	m_OutImg = (unsigned char**) malloc(ImgSize);
+	for (int i = 1; i < height; i++) {
+		m_OutImg[i] = m_OutImg[i - 1] + width;
+	}
+	memset(m_OutImg[0], 0, ImgSize);*/
+
+	hFile.Read(m_InImg, ImgSize);
+	hFile.Close();
+
+	
+
+	return TRUE;
+}
+
+
+BOOL Cbmpkyh2Doc::OnSaveDocument(LPCTSTR lpszPathName)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+
+	// 전환
+	unsigned char **temp;
+	int height = dibHi.biHeight;
+	int width = dibHi.biWidth;
+
+	// BMP는 아래에서 위로 저장되는 형식이기 때문에 순서대로 저장하면 뒤집히므로 파일로 저장할 데이터를 뒤집기
+	SPixel* rgbTemp = new SPixel[BUFSIZE]; // 파일저장할 데이터를 보관할 임시공간
+	
+	for (int i = 0; i < BUFSIZE; i++) {
+		rgbTemp[i].r = g_rgb[BUFSIZE - 1 - i].r;
+		rgbTemp[i].g = g_rgb[BUFSIZE - 1 - i].g;
+		rgbTemp[i].b = g_rgb[BUFSIZE - 1 - i].b;
+	}
+
+
+	// bmp file 저장
+	CFile file;
+	file.Open(_T("bmp_re.bmp"), CFile::modeCreate | CFile::modeWrite | CFile::typeBinary);
+	file.Write(&dibHf, sizeof(BITMAPFILEHEADER));
+	file.Write(&dibHi, sizeof(BITMAPINFOHEADER));
+	if (dibHi.biBitCount == 8)
+		file.Write(palRGB, sizeof(RGBQUAD) * 256);
+	file.Write(temp, height*width);
+	file.Close();
+	free(m_InImg);
+
+	return TRUE;
+}
---
+++ bmp_kyh_2View.cpp
@@ -0,0 +1,209 @@
+﻿
+// bmp_kyh_2View.cpp: Cbmpkyh2View 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "bmp_kyh_2.h"
+#endif
+
+#include "bmp_kyh_2Doc.h"
+#include "bmp_kyh_2View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Cbmpkyh2View
+
+IMPLEMENT_DYNCREATE(Cbmpkyh2View, CScrollView)
+
+BEGIN_MESSAGE_MAP(Cbmpkyh2View, CScrollView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &Cbmpkyh2View::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// Cbmpkyh2View 생성/소멸
+
+Cbmpkyh2View::Cbmpkyh2View()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	BmInfo = (BITMAPINFO*)malloc(sizeof(BITMAPINFO) + 256 * sizeof(RGBQUAD));
+
+	for (int i = 0; i < 256; i++)
+	{
+		BmInfo->bmiColors[i].rgbRed = BmInfo->bmiColors[i].rgbGreen = BmInfo->bmiColors[i].rgbReserved = 0;
+	}
+}
+
+Cbmpkyh2View::~Cbmpkyh2View()
+{
+	if (BmInfo) delete BmInfo;
+}
+
+BOOL Cbmpkyh2View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CScrollView::PreCreateWindow(cs);
+}
+
+// Cbmpkyh2View 그리기
+
+void Cbmpkyh2View::OnDraw(CDC* pDC)
+{
+#define WIDTHBYTES(bits)	(((bits)+31)/32*4);        //이미지 가로 바이트 길이는 4바이트의 배수
+
+	//BmInfo;
+
+	int height;
+
+	int width;
+
+	int rwsize;
+
+
+
+	CBmpReadingDoc* pDoc = GetDocument();
+
+	ASSERT_VALID(pDoc);
+
+	if (!pDoc)
+
+		return;
+
+
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+
+	if (pDoc->m_InImg == NULL)
+
+		return;
+
+
+
+	//24비트 비트맵 파일의 영상출력
+
+	if (pDoc->dibHi.biBitCount == 24) {
+
+		height = pDoc->dibHi.biHeight;
+		width = pDoc->dibHi.biWidth;
+		rwsize = WIDTHBYTES(pDoc->dibHi.biBitCount*pDoc->dibHi.biWidth);
+
+		BmInfo->bmiHeader = pDoc->dibHi;
+		SetDIBitsToDevice(pDC->GetSafeHdc(), 0, 0, width, height, 0, 0, 0, height, pDoc->m_InImg, BmInfo, DIB_RGB_COLORS);
+
+	}
+
+	else	//8비트 컬러일 경우
+
+	{
+		int index;
+		rwsize = WIDTHBYTES(pDoc->dibHi.biBitCount*pDoc->dibHi.biWidth);
+
+		//팔레트를 읽어들이며 반복출력
+		for (int i = 0; i < pDoc->dibHi.biHeight; i++)
+			for (int j = 0; j < pDoc->dibHi.biWidth; j++) 
+			{
+				index = pDoc->m_InImg[i*rwsize + j];
+				BYTE R = pDoc->palRGB[index].rgbRed;
+				BYTE G = pDoc->palRGB[index].rgbGreen;
+
+				BYTE B = pDoc->palRGB[index].rgbBlue;
+
+
+
+				//pDC->SetPixel(j,i,RGB(R,G,B));
+				//영상 반전출력
+
+				pDC->SetPixel(j, pDoc->dibHi.biHeight - i - 1, RGB(R, G, B));
+
+
+
+			}
+
+	}
+
+}
+
+void Cbmpkyh2View::OnInitialUpdate()
+{
+	CScrollView::OnInitialUpdate();
+
+	CSize sizeTotal;
+	// TODO: 이 뷰의 전체 크기를 계산합니다.
+	sizeTotal.cx = sizeTotal.cy = 100;
+	SetScrollSizes(MM_TEXT, sizeTotal);
+}
+
+
+// Cbmpkyh2View 인쇄
+
+
+void Cbmpkyh2View::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL Cbmpkyh2View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void Cbmpkyh2View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void Cbmpkyh2View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void Cbmpkyh2View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void Cbmpkyh2View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// Cbmpkyh2View 진단
+
+#ifdef _DEBUG
+void Cbmpkyh2View::AssertValid() const
+{
+	CScrollView::AssertValid();
+}
+
+void Cbmpkyh2View::Dump(CDumpContext& dc) const
+{
+	CScrollView::Dump(dc);
+}
+
+Cbmpkyh2Doc* Cbmpkyh2View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Cbmpkyh2Doc)));
+	return (Cbmpkyh2Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// Cbmpkyh2View 메시지 처리기
